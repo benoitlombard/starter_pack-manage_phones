48416a16a2161c1cfae90185d56f70267bea8289
benoitlombard <b.lombard@technologyandstrategy.com>
2026-04-14 15:28:22 +0200

    last_file_of_separeted_functions_in_files

---
+++ change_phone.py
@@ -0,0 +1,50 @@
+from manage_phones import *
+
+# change phone
+def change_phone(yaml_d: dict)->None:
+	"""
+	Allows user to change some informations from 'phones' data by asking which phone and what value of attribute he want to change
+	"""
+	dict_of_phones = {}
+	print('Phone to change: ')
+	for phone_index, phone in  enumerate(yaml_d['phones']):
+		dict_of_phones[phone_index] = phone
+		print(str(phone_index) + ': ' + yaml_d['phones'][phone]['name'])
+		print('\t' + yaml_d['phones'][phone]['vendor'] + ' ' + yaml_d['phones'][phone]['family'] + ' ' + str(yaml_d['phones'][phone]['version']))
+	indx = input('? ')
+	phone = dict_of_phones[int(indx)]
+	yaml.dump(phone, sys.stdout)
+	print('What to change')
+	print('1: Release type')
+	print('2: User')
+	print('3: Testrun id\'s')
+	ret = input('? ')
+	if ret == '1':
+		print('Release type:')
+		print('1: PU1')
+		print('2: PU100')
+		ret = input('? ')
+		while ret != '1' and ret != '2':
+			print('Please select a for PU1 or b for PU100')
+			ret = input('? ')
+		if ret == '1':
+			releasetype = 'PU1'
+		elif ret == '2':
+			releasetype = 'PU100'
+		yaml_d['phones'][phone]['release_type'] = releasetype
+	elif ret == '2':
+		print('New user: (complete string)')
+		user = input('? ')
+		yaml_d['phones'][phone]['user'] = user
+	elif ret == '3':
+		fota = input('fota: ')
+		activitytracking = input('activitytracking: ')
+		functional = input('functional: ')
+		performance = input('performance: ')
+		testrun_ids = dict(fota = fota, activitytracking = activitytracking, functional = functional, performance = performance)
+		yaml_d['phones'][phone]['testrun_ids'] = testrun_ids
+	else:
+		print('Unknown selection')
+
+	with open(file_name, 'w') as w:
+		yaml.dump(yaml_d, w)
--- manage_phones.py
+++ manage_phones.py
@@ -1,6 +1,6 @@
 # importing libraries
 import ruamel.yaml
-import sys
+import sys # useful for dumping yaml content to terminal
 import click
 
 file_name = 'test.yaml'
@@ -11,61 +11,10 @@ with open(file_name, 'r') as yaml_file:
 # importing other files
 import display_infos
 import add_phone
+import change_phone
 import deploy_phone
 import remove_undeploy_phone
 
-# change phone
-def change_phone()->None:
-	"""
-	Allows user to change some informations from 'phones' data by asking which phone and what value of attribute he want to change
-	"""
-	dict_of_phones = {}
-	print('Phone to change: ')
-	for phone_index, phone in  enumerate(yaml_d['phones']):
-		dict_of_phones[phone_index] = phone
-		print(str(phone_index) + ': ' + yaml_d['phones'][phone]['name'])
-		print('\t' + yaml_d['phones'][phone]['vendor'] + ' ' + yaml_d['phones'][phone]['family'] + ' ' + str(yaml_d['phones'][phone]['version']))
-	indx = input('? ')
-	phone = dict_of_phones[int(indx)]
-	yaml.dump(phone, sys.stdout)
-	print('What to change')
-	print('1: Release type')
-	print('2: User')
-	print('3: Testrun id\'s')
-	ret = input('? ')
-	if ret == '1':
-		print('Release type:')
-		print('1: PU1')
-		print('2: PU100')
-		ret = input('? ')
-		while ret != '1' and ret != '2':
-			print('Please select a for PU1 or b for PU100')
-			ret = input('? ')
-		if ret == '1':
-			releasetype = 'PU1'
-		elif ret == '2':
-			releasetype = 'PU100'
-		yaml_d['phones'][phone]['release_type'] = releasetype
-	elif ret == '2':
-		print('New user: (complete string)')
-		user = input('? ')
-		yaml_d['phones'][phone]['user'] = user
-	elif ret == '3':
-		fota = input('fota: ')
-		activitytracking = input('activitytracking: ')
-		functional = input('functional: ')
-		performance = input('performance: ')
-		testrun_ids = dict(fota = fota, activitytracking = activitytracking, functional = functional, performance = performance)
-		yaml_d['phones'][phone]['testrun_ids'] = testrun_ids
-	else:
-		print('Unknown selection')
-
-	with open(file_name, 'w') as w:
-		yaml.dump(yaml_d, w)
-
-
-
-		
 
 if __name__ == "__main__":
 	ret = ''
@@ -79,14 +28,11 @@ if __name__ == "__main__":
 		print('5: Remove phone')
 		print('6: Show configuration')
 		print('x: Exit')
-		with open(file_name, 'r') as yaml_file:
-			yaml = ruamel.yaml.YAML(typ='rt')
-			yaml_d = yaml.load(yaml_file)
 		ret = input('? ')
 		if ret == '1':
 			add_phone.add_phone(yaml_d)
 		elif ret == '2':
-			change_phone()
+			change_phone.change_phone(yaml_d)
 		elif ret == '3':
 			deploy_phone.deploy_phone(yaml_d)
 		elif ret == '4':
@@ -95,3 +41,4 @@ if __name__ == "__main__":
 			remove_undeploy_phone.remove_phone(yaml_d)
 		elif ret == '6':
 			display_infos.display(yaml_d)
+
