a311a75d3634f3c9e2ca7f67789e7dc5605596d6
benoitlombard <b.lombard@technologyandstrategy.com>
2026-04-30 14:54:08 +0200

    build_119_writing_test_w_pytest

--- app/test_file.py
+++ app/test_file.py
@@ -1,10 +1,22 @@
 from manage_phones_CLI import add, change, deploy, undeploy, remove, show_config, lists, phone_management_app
 import pytest
+import ruamel.yaml
 
 
-def test_001_deploy():
-    assert 2 + 2 == 4
-    return "whatever i returned"
+#######   WILL NEED TO BE IMPORTED   #######
+
+file_name = 'test.yaml'
+with open(file_name, 'r') as yaml_file:
+    yaml = ruamel.yaml.YAML(typ='rt')
+    yaml_d = yaml.load(yaml_file)
+
+
+
+def test_001_deploy(capsys):
+    exit_code = deploy(phone = "Chaos", stage = 'dev')
+    captured = capsys.readouterr()
+    print(captured)
+    assert exit_code == None
 
 
 
