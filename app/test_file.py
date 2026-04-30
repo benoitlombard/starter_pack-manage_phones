b19a9d0122a3ca95a391769974ce1a718b248417
benoitlombard <b.lombard@technologyandstrategy.com>
2026-04-30 15:19:39 +0200

    build_121_more_complex_pytest

--- app/test_file.py
+++ app/test_file.py
@@ -12,15 +12,43 @@ with open(file_name, 'r') as yaml_file:
 
 
 
-def test_001_deploy(capsys):
+def test_001_deploy():  # verifying deploy 
     exit_code = deploy(phone = "Chaos", stage = 'dev')
+    assert exit_code == None
+
+
+
+
+
+
+# test cqtching err
+def test_002_deploy(capsys):                                            ###### !!!
+    exit_code = deploy(phone = "wrong_phone_name", stage = 'dev')
     captured = capsys.readouterr()
     print(captured)
-    assert exit_code == 8
-    return "001 fails"
+    assert exit_code == None
 
 
 
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def test_greet_output(capsys):
+    # Call the function that prints
+    greet("Alice")
+
+    # Capture the output
+    captured = capsys.readouterr()
+
+    # Assert on stdout
+    assert captured.out == "Hello, Alice!\n"
+    # Assert stderr if needed
+    assert captured.err == ""
+
+
+test_001_deploy()
 
 
 
