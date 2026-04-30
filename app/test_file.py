90387bda51530718a1fa9199f19b670f7c0dbf6a
benoitlombard <b.lombard@technologyandstrategy.com>
2026-04-30 16:22:00 +0200

    build_125_more_organized_tests_w_pytest

--- app/test_file.py
+++ app/test_file.py
@@ -1,32 +1,47 @@
 from manage_phones_CLI import add, change, deploy, undeploy, remove, show_config, lists, phone_management_app
 import pytest
-import ruamel.yaml
-
-
 
 from manage_phones import yaml_d, yaml, file_name
 
 
+"""
+The chosen naming rule for test functions is:   'test'  +  id(from 001)  +  __  +  function used  +  __  +  important parameters or 'ok' if normal use  +  __  +  asserted features
+                               exemples :       
+                                            test001__undeploy__ok__yaml_attributes
+                                            test002__deploy__ok__yaml_attributes
+                                            test003__undeploy__ok__terminal_output
+"""
 
-
-def test001_undeploy_good_infos(capsys):  # verifying deploy 
+def test001__undeploy__ok__yaml_attributes():
     exit_code = undeploy(phone = "Chaos")
+
     hub = yaml_d['phones']['Chaos']['deployment_path']['hub']
     port = yaml_d['phones']['Chaos']['deployment_path']['port']
-
-    captured = capsys.readouterr()
-    print(captured.out)
-
     assert exit_code == None
     assert hub == None and port == None
+
+def test002__deploy__ok__yaml_attributes():
+    exit_code = deploy(phone = "Chaos", stage = 'dev')
+
+    hub = yaml_d['phones']['Chaos']['deployment_path']['hub']
+    port = yaml_d['phones']['Chaos']['deployment_path']['port']
+    deployment_path = yaml_d['stage']['dev'][hub][port]
+    assert exit_code == None
+    assert hub != None and port != None
+    assert deployment_path == yaml_d['phones']['Chaos']
+
+def test003__undeploy__ok__output(capsys):
+    undeploy(phone = "Chaos")
+
+    captured = capsys.readouterr()
     assert "Chaos successfully undeployed." in captured.out
     assert "Please unplug Chaos from port12 at hub tp_link_tlsg3424" in captured.out
 
-def test002_deploy_good_infos():  # verifying deploy 
-    exit_code = deploy(phone = "Chaos", stage = 'dev')
-    assert exit_code == None
+def test004__deploy__ok__output(capsys):
+    deploy(phone = "Chaos", stage = 'dev')
 
-
+    captured = capsys.readouterr()
+    assert "Chaos successfully deployed in dev." in captured.out
 
 
 
