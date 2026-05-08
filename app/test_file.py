fc9fc00f82e96725f3874f88debce49392ed09fe
benoitlombard <b.lombard@technologyandstrategy.com>
2026-05-08 14:56:46 +0200

    fully_working_build_232

--- app/test_file.py
+++ app/test_file.py
@@ -3,177 +3,10 @@ import pytest
 
 from manage_phones import yaml_d, yaml, file_name
 
-""" The naming rule for test functions is:   'test_'  +  id(from 001)  +  __  +  function used  +  __  +  important parameters or 'ok' if normal use  +  __  +  asserted features
-                            exemples :       
-                                            test001__undeploy__ok__yaml_attributes
-                                            test002__deploy__ok__yaml_attributes
-                                            test003__undeploy__ok__terminal_output
-                                            test004__deploy__ok__output
 """
-def test_001__undeploy__ok__yaml_attributes():
-    exit_code = undeploy(phone = "Chaos")
-
-    hub = yaml_d['phones']['Chaos']['deployment_path']['hub']
-    port = yaml_d['phones']['Chaos']['deployment_path']['port']
-    assert exit_code == None
-    assert hub == None and port == None
-
-def test_002__deploy__ok__yaml_attributes():
-    phone, stage = "Chaos", 'dev'
-    exit_code = deploy(phone = phone, stage = stage)
-
-    hub = yaml_d['phones'][phone]['deployment_path']['hub']
-    port = yaml_d['phones'][phone]['deployment_path']['port']
-    assert exit_code == None
-    assert hub != None and port != None
-
-    hub_nb = 0
-    for hub_number in range(len(yaml_d['stages'][stage])):
-        if yaml_d['stages'][stage][hub_number]['name'] == hub:
-            hub_nb = hub_number
-    deployment_path = yaml_d['stages'][stage][hub_nb][port]
-    assert deployment_path == yaml_d['phones'][phone]
-
-def test_003__undeploy__ok__output(capsys):
-    undeploy(phone = "Chaos")
-
-    captured = capsys.readouterr()
-    assert "Chaos successfully undeployed." in captured.out
-    assert "Please unplug Chaos from port01 at hub exsys_ex_1116hmvs_1" in captured.out
-
-def test_004__deploy__ok__output(capsys):
-    phone, stage = "Chaos", 'dev'
-    deploy(phone = phone, stage = stage)
-
-    captured = capsys.readouterr()
-    assert "Chaos successfully deployed in dev." in captured.out
-
-def test_005__deploy__incorrect_stage__yaml_attributes_and_output(capsys):
-    phone, stage = "Chaos", 'incorrect_stage'
-    undeploy(phone = phone)
-    exit_code = deploy(phone = phone, stage = stage)
-
-    hub = yaml_d['phones']['Chaos']['deployment_path']['hub']
-    port = yaml_d['phones']['Chaos']['deployment_path']['port']
-    assert exit_code == None
-    assert hub == None and port == None
-
-    phone_found_in_stages = False
-    for stage in ['dev', 'prod']:
-        for hub_number in range(len(yaml_d['stages'][stage])):
-            for port in yaml_d['stages'][stage][hub_number]:
-                if yaml_d['stages'][stage][hub_number][port] == yaml_d['phones'][phone]:
-                    phone_found_in_stages = True
-    assert not phone_found_in_stages
-    captured = capsys.readouterr()
-    assert "Please select a stage from" in captured.out
-
-def test_006__deploy__phone_already_deployed__output(capsys):
-    phone, stage = "Chaos", "dev"
-    deploy(phone = phone, stage = stage)
-    deploy(phone = phone, stage = stage)
-
-    captured = capsys.readouterr()
-    assert f'{phone} is already deployed.' in captured.out
-
-def test_007__deploy__incorrect_phone_name__output_and_error(capsys):
-    phone, stage = "incorrect_name", "dev"
-
-    with pytest.raises(KeyError):
-        deploy(phone = phone, stage = stage)
-    captured = capsys.readouterr()
-    assert f'No phone named {phone}.' in captured.out
-
-def test_008__undeploy__incorrect_phone_name__output_and_error(capsys):
-    phone = "incorrect_name"
-
-    with pytest.raises(KeyError):
-        undeploy(phone = phone)
-    captured = capsys.readouterr()
-    assert "Key Error when writing to the yaml file.\nUndeployment failed, please verify phone name." in captured.out
-
-def test_009__undeploy__not_deployed_phone__output(capsys):
-    phone = "Hesperiden"
-    undeploy(phone = phone)
-
-    captured = capsys.readouterr()
-    assert f"{phone} is not deployed.\nUndeployment is not possible." in captured.out
-
-def test_010__add__ok_minimal_infos__output(capsys):
-    vendor = 'apple'
-    family = 'ios4'
-    version = '5.6'
-    udid = 'udid_1'
-    release_type = 'PU100'
-    add(vendor = vendor, family = family, version = version, udid = udid, release_type = release_type)
-
-    captured = capsys.readouterr()
-    assert "RTC device name: " in captured.out
-    assert "Platform set to: " in captured.out
-    assert "IP used: " in captured.out
-    assert " successfully added." in captured.out
-
-def test_011__add__ok_minimal_infos__yaml_attributes():
-    vendor = 'apple'
-    family = 'ios4'
-    version = '5.6'
-    udid = 'udid_2'
-    release_type = 'PU100'
-    add(vendor = vendor, family = family, version = version, udid = udid, release_type = release_type)
-
-    dict_attributes_found_in_yaml = {'vendor': None, 'family': None, 'version': None, 'udid': None, 'release_type': None}
-    for phone in yaml_d['phones']:
-        if yaml_d['phones'][phone]['udid'] == udid:
-            for attribute_key in dict_attributes_found_in_yaml.keys():
-                dict_attributes_found_in_yaml[attribute_key] = yaml_d['phones'][phone][attribute_key]
-    
-    dict_attributes = {'vendor': vendor, 'family': family, 'version': version, 'udid': udid, 'release_type': release_type}
-    for key, value_found_in_yaml in dict_attributes_found_in_yaml.items():
-        assert value_found_in_yaml == dict_attributes[key]
-
-def test_012__add__ok_with_testrun_ids__output(capsys):
-    vendor = 'apple'
-    family = 'ios4'
-    version = '5.6'
-    udid = 'udid_3'
-    release_type = 'PU100'
-    fota = 'fota_id'
-    activitytracking = '3a4-e1c'
-    functional = '65'
-    performance = '124'
-    add(vendor = vendor, family = family, version = version, udid = udid, release_type = release_type, fota = fota, activitytracking = activitytracking, functional = functional, performance = performance)
-
-    captured = capsys.readouterr()
-    assert "RTC device name: " in captured.out
-    assert "Platform set to: " in captured.out
-    assert "IP used: " in captured.out
-    assert " successfully added." in captured.out
-
-def test_013__add__ok_testrun_ids__yaml_attributes():
-    vendor = 'apple'
-    family = 'ios4'
-    version = '5.6'
-    udid = 'udid_4'
-    release_type = 'PU100'
-    fota = 'fota_id'
-    activitytracking = 'activitytracking_id'
-    functional = '65'
-    performance = '124'
-    add(vendor = vendor, family = family, version = version, udid = udid, release_type = release_type, fota = fota, activitytracking = activitytracking, functional = functional, performance = performance)
-
-    dict_attributes_found_in_yaml = {'vendor': None, 'family': None, 'version': None, 'udid': None, 'release_type': None, 'fota': None, 'activitytracking': None, 'functional': None, 'performance': None}
-    for phone in yaml_d['phones']:
-        if yaml_d['phones'][phone]['udid'] == udid:
-            print('trouve', yaml_d['phones'][phone])
-            for attribute_key in ['vendor', 'family', 'version', 'udid', 'release_type']:
-                dict_attributes_found_in_yaml[attribute_key] = yaml_d['phones'][phone][attribute_key]
-            for attribute_key in ['fota', 'activitytracking', 'functional', 'performance']:
-                dict_attributes_found_in_yaml[attribute_key] = yaml_d['phones'][phone]['testrun_ids'][attribute_key]
-    
-    dict_attributes = {'vendor': vendor, 'family': family, 'version': version, 'udid': udid, 'release_type': release_type, 'fota': fota, 'activitytracking': activitytracking, 'functional': functional, 'performance': performance}
-    for key, value_found_in_yaml in dict_attributes_found_in_yaml.items():
-        assert value_found_in_yaml == dict_attributes[key]
-
+    The following functions verify data attributions, outputs and errors using pytest
+    Every possible output is reached.
+"""
 
 def _asserting_phone_informations_in_stdout(captured, phone: str):
     assert f"name: {phone}" in captured.out
@@ -451,7 +284,7 @@ def test_show_config(capsys, phone: str):
 @pytest.mark.parametrize("item_to_show,stage_to_show", [("incorrect_item", ""), ("incorrect_item", "incorrect_stage"),("phones", "incorrect_stage"),
                                         ("phones", ""), ("bts", ""), ("biab", ""), ("stage", "dev"), ("stage", "prod"), ("undeployed_phones", ""),
                                         ("phones", "incorrect_stage"), ("bts", "incorrect_stage"), ("biab", "incorrect_stage"), ("stage", "incorrect_stage"),
-                                        ("stage", "incorrect_stage"), ("undeployed_phones", "incorrect_stage")])
+                                        ("stage", "incorrect_stage"), (4, 4),(4, "dev"), ("stage", 4), ("phones", 4), ("biab", 4), ("bts", 4)])
 def test_lists_phones(capsys, item_to_show: str, stage_to_show: str):
     match item_to_show.lower():
         case 'stage':
