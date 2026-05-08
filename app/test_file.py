c73b3c0175bb20656e22a2fc2912d5684d8b171e
benoitlombard <b.lombard@technologyandstrategy.com>
2026-05-08 16:33:05 +0200

    fixing

--- app/test_file.py
+++ app/test_file.py
@@ -374,8 +374,9 @@ def test_change_phone(capsys, phone: str, release_type: str, user: str, fota: st
                           ("apple", "ios3", "4.2", "unique_udid_8", "user_n", "PU1", True, "fota_id", "activitytracking_id", "functional_id", "performance_id", "apple", "model_2"),
                           ("apple", "ios3", "4.2", "unique_udid_8", "user_n", "PU1", True, "fota_id", "", "", "", "", ""),                                          # case : udid already used
                           ("apple", "ios3", "4.2", "unique_udid_9", "user_n", "PU1", False, "fota_id", "", "", "", "", ""),
-                          ("apple", "ios3", "4.2", "unique_udid_10", "user_n", "PU1", True, "fota_id", "", "", "", "", "model_n"),
-                          ("samsung", "SG8", "20", "unique_udid_12", "user_n", "PU1", True, 4, 4, 4, 4, "4", 4)])                      # case: not enough ips available
+                          ("apple", "ios3", "4.2", "unique_udid_10", "user_n", "PU1", False, "fota_id", "", "", "", "", "model_n"),
+                          ("apple", "ios3", "4.2", "unique_udid_11", "user_n", "PU1", False, "fota_id", "", "", "", "", ""),
+                          ("samsung", "SG8", "20", "unique_udid_12", "user_n", "PU1", True, "fota_id", "", "", "", "", "model_2")])                      # case: not enough ips available
 def test_add_phone(capsys, vendor: str, family: str, version: str, udid: str, user: str, release_type: str, write: bool, fota: str,
                    activitytracking: str, functional: str, performance: str, manufacturer: str, model: str):
     is_udid_used = False
@@ -437,7 +438,7 @@ def test_add_phone(capsys, vendor: str, family: str, version: str, udid: str, us
             assert "successfully added, but not saved in yaml file" in captured.out
 
 @pytest.mark.show_phone_configuration
-@pytest.mark.parametrize("phone", [("Nyx"), ("Chaos"), ("Hemera"), ("incorrect_phone")])
+@pytest.mark.parametrize("phone", [("Nyx"), ("Chaos"), ("incorrect_phone")])
 def test_show_config(capsys, phone: str):
     show_config(phone = phone)
     captured = capsys.readouterr()
